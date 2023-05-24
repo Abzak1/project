5ea5e3131ed852946c825381c42696c10fc81c16
TelvinHoward20 <real4real19@gmail.com>
2023-05-24 19:45:55 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,24 @@
+
+// let btn = document.querySelector(".btn-contact")
+// btn.addEventListener("click",function(){
+//     let nameInput = document.getElementById("name")
+//     if(nameInput.value == ""){
+//         nameInput.style.borderColor ="#F00" 
+//     }
+//     else{
+//         nameInput.style.borderColor="green"
+//     }
+
+// })
+// let price1=45.00;
+// let item1=document.getElementById("vanSelect");
+// let total=document.getElementById("total");
+// total.value=parseInt(item1.value)*parseInt(price1);
+
+
+" use strict";
+
+const formWrapper=document.querySelector('.wrapper');
+
+console.log(formWrapper);
+
